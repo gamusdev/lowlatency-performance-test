666b351dab87aba55b7e27d1297c44e15714e77b
DRamirez <dramirez@gamusdev.com>
2022-05-20 18:44:00 +0200

    Init brokers module

---
+++ brokers/src/test/java/com/gamusdev/lowlatency/performance/tests/brokers/BrokersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamusdev.lowlatency.performance.tests.brokers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrokersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
