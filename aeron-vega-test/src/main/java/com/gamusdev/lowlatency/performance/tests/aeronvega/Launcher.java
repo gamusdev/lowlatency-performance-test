bb6548eb432f36fcc3f05c358eb8abac487f7ea2
DRamirez <dramirez@gamusdev.com>
2022-05-22 20:45:39 +0200

    ITestExecutor loaded dynamically

--- aeron-vega-test/src/main/java/com/gamusdev/lowlatency/performance/tests/aeronvega/Launcher.java
+++ aeron-vega-test/src/main/java/com/gamusdev/lowlatency/performance/tests/aeronvega/Launcher.java
@@ -1,9 +1,12 @@
 package com.gamusdev.lowlatency.performance.tests.aeronvega;
 
 import com.gamusdev.lowlatency.performance.tests.aeronvega.exception.GenericAeronVegaException;
-import com.gamusdev.lowlatency.performance.tests.aeronvega.executor.VegaTestExecutor;
+import com.gamusdev.lowlatency.performance.tests.aeronvega.executor.ITestExecutor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.cli.ParseException;
+
+import java.util.Optional;
+import java.util.ServiceLoader;
 
 
 /**
@@ -15,8 +18,20 @@ public class Launcher {
     public static void main(String[] args)
             throws ParseException, GenericAeronVegaException {
 
-        // Execute the desired tests
-        new VegaTestExecutor().executeTest(args);
+
+        /**
+         * Initialize the TestExecutor using Java 11 ServiceLoader.
+         * The service loader uses META-INF/services/ files to inject the services
+         * See: https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/ServiceLoader.html
+         */
+        Optional<ITestExecutor> optTestExecutor = ServiceLoader.load(ITestExecutor.class).findFirst();
+
+        if (optTestExecutor.isPresent()) {
+            // Execute the desired tests
+            optTestExecutor.get().executeTest(args);
+        } else {
+            log.error("ITestExecutor NOT FOUND");
+        }
 
     }
 }
